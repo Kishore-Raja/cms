62cca12eb71b9ce7e055f557604455a887278ef4
Kishore Raja <kishore.raja01@gmail.com>
2023-03-10 15:28:17 +0530

    error handling for fetch api

--- src/App.js
+++ src/App.js
@@ -7,28 +7,48 @@ function App() {
 
   
   const [user, setUser] = useState(null)
+  const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(false)
 
-  const promise1 = new Promise((resolve,reject) => {
+  const promise1 = new Promise((resolve) => {
     fetch("http://localhost:8000/users")
-      .then(res=>res.json())
+      .then(res=>{
+        if(!res.ok){
+          throw Error("Failure: retry after some time")
+        }
+        return res.json()
+      })
       .then(data => resolve(data.blogs))
+      .catch( err=>{
+        setIsPending(false)
+        setError(err.message)
+      })
   });
   
 
-  const promise2 = new Promise((resolve,reject) => {
-    fetch("http://localhost:8000/posts")
-      .then(res=>res.json())
+  const promise2 = new Promise((resolve) => {
+    fetch("http://localhost:8000/posts1")
+      .then(res=>{
+        if(!res.ok){
+          throw Error("Failure: retry after some time")
+        }
+        return res.json()
+      })
       .then(data => resolve(data.blogs))
+      .catch( err=>{
+        setIsPending(false)
+        setError(err.message)
+      })
   });
   
  
   useEffect(()=>{
-    console.log("useEffect")
     Promise.all([promise1, promise2])
       .then((results)=>{
         let finalData = [...results[0],...results[1]]
        setUser(finalData)
-     })
+       setIsPending(false)
+     }).catch(err=>console.log(err))
    
   },[])
   
@@ -36,6 +56,8 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="content">
+        {isPending && <div className="loading">Loading...</div> }
+        {error? error: ""}
         {user && <Home user={user} />}
       </div>
     </div>
